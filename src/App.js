c3ebad99427ca842cf62a08e6d4c5d6bc02ac323
sourav p <souravpadmaraj6@gmail.com>
2022-03-03 15:43:49 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "bootstrap/dist/css/bootstrap.css";
+import "./App.css";
+import Routes from "./component/Routes";
+
+function App() {
+  return (
+    <>
+      <Routes />
+    </>
+  );
+}
+
+export default App;
---
+++ src/component/Login.test.js
@@ -0,0 +1,34 @@
+/* eslint-disable testing-library/prefer-screen-queries */
+
+import { fireEvent, queryAllByTestId, render} from "@testing-library/react"
+// import { fireEvent } from "@testing-library/react"
+import Login from "./Login"
+
+
+describe("input field test for email",()=>{
+    it("login render",()=>{
+        let {queryAllByTestId}=render (<Login />)
+        let input = queryAllByTestId("email")
+        expect(input).toBeTruthy()
+    })
+    it("input onChange",()=>{
+        let {queryAllByTestId}=render (<Login />)
+        let input = queryAllByTestId("email")
+        fireEvent.change(input,{target:{value:"testValue"}})
+        expect(input.value).toBe("testValue")
+    })
+})
+
+describe("input field test for password",()=>{
+    it("login render",()=>{
+        let {queryAllByTestId}=render (<Login />)
+        let input = queryAllByTestId("password")
+        expect(input).toBeTruthy()
+    })
+    it("input onChange",()=>{
+        let{queryAllByTestId}=render (<Login />)
+        let input = queryAllByTestId("password")
+        fireEvent.change(input,{target:{value:"testValue"}})
+        expect(input.value).toBe("testValue")
+    })
+})
